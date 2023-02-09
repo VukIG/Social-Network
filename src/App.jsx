d8f55c968fc413175b522c3c4561150366476b45
VukIG <vuk.ignjatovic05@gmail.com>
2023-02-08 18:29:32 -0900

    ne radi firebase ni tailwind nzm sto

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Router>
-        <nav>
-          <Link></Link>
-        </nav>
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </Router>
+     <h1 text-white mt-5>Jovan</h1>
     </div>
   )
 }
